2285a754c4b4ebe1a9ab4bd20efb29e5c94e1697
Suraj Jana <surajjana2@gmail.com>
2016-06-02 01:58:51 +0530

    new commit

--- hiq/js/event.js
+++ hiq/js/event.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 
 $.fn.Show = function(id,size) {
+
+	/*$("#btn_"+id).removeClass("btn-info").addClass("btn-default");*/
         
         for(i=1;i<=size;i++){
         	if(i != id){
+        		/*if($('#btn_'+i).hasClass('btn-default')){*/
+        		$("#btn_"+i).removeClass("btn-danger").addClass("btn-info");
         		$('#'+i).css("display","none");
         	}else{
+        		$("#btn_"+id).removeClass("btn-info").addClass("btn-danger");
         		$('#'+id).css("display","block");
         	}
         }
