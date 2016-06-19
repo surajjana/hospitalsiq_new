ca8d67f3a74079f098ec664dfb5eb17b61f4019d
Suraj Jana <surajjana2@gmail.com>
2016-06-19 13:23:36 +0530

    new commit

--- hiq/hosp.php
+++ hiq/hosp.php
@@ -24,6 +24,8 @@ $data = json_decode($data, true);
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -31,6 +33,137 @@ $data = json_decode($data, true);
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
 	<script type="text/javascript" src="js/event.js"></script>
+
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+	<style type="text/css">
+
+        #list_btn{
+            width: 30px;
+            height: 30px;
+            float: right;
+            background-color: #fafafa;
+            border: none;
+            border-radius: 50%;
+            position: relative;
+            top: -55px;
+            left: -50px;
+            box-shadow: 0 1px 2px;
+        }
+
+        #map_btn{
+            width: 30px;
+            height: 30px;
+            float: right;
+            background-color: #fafafa;
+            border: none;
+            border-radius: 50%;
+            position: fixed;
+            box-shadow: 0 1px 2px;
+            top: 600px;
+            right: 200px;
+        }
+
+        #map_btn_mob{
+            width: 30px;
+            height: 30px;
+            float: right;
+            background-color: #fafafa;
+            border: none;
+            border-radius: 50%;
+            position: fixed;
+            box-shadow: 0 1px 2px;
+            top: 500px;
+            right: 15px;
+        }
+
+    </style>
+
+    <script type="text/javascript">
+        var contentstring = [<?php for($i=0;$i<sizeof($data["result"]);$i++){if($i == sizeof($data["result"])-1){echo '"'.$data["result"][$i]["name"].'"';}else{echo '"'.$data["result"][$i]["name"].'",';}} ?>];
+        var regionlocation = [<?php for($i=0;$i<sizeof($data["result"]);$i++){if($i == sizeof($data["result"])-1){echo '"'.$data["result"][$i]["latitude"].', '.$data["result"][$i]["longitude"].'"';}else{echo '"'.$data["result"][$i]["latitude"].', '.$data["result"][$i]["longitude"].'",';}} ?>];
+        var markers = [];
+        var iterator = 0;
+        var areaiterator = 0;
+        var map;
+        var infowindow = [];
+        geocoder = new google.maps.Geocoder();
+        
+        $(document).ready(function () {
+            setTimeout(function() { initialize(); }, 400);
+
+            $('#list_btn').click(function(){
+                $('#map').css("display", "none");
+                $('#list').css("display", "block");
+                console.log('List Switch Button Clicked...');
+            });
+            $('#map_btn').click(function(){
+                $('#list').css("display", "none");
+                $('#map').css("display", "block");
+                console.log('Map Switch Button Clicked...');
+            });
+            $('#map_btn_mob').click(function(){
+                $('#list').css("display", "none");
+                $('#map').css("display", "block");
+                console.log('Map Switch Button Clicked...');
+            });
+        });
+        
+        function initialize() {           
+            // infowindow = [];
+            // markers = [];
+            //GetValues();
+            /*iterator = 0;
+            areaiterator = 0;*/
+            region = new google.maps.LatLng(<?php echo $_POST['lat'].','.$_POST['lon']; ?>);
+            /*console.log(region.lat());*/
+            map = new google.maps.Map(document.getElementById("Map"), { 
+                zoom: 12,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                center: region,
+            });
+            drop();
+        }
+               
+        function drop() {
+            for (var i = 0; i < contentstring.length; i++) {
+                setTimeout(function() {
+                    addMarker();
+                }, 800);
+            }
+        }
+
+        function addMarker() {
+            var address = contentstring[areaiterator];
+            var icons = './img/hiq_map_marker.png';
+            var templat = regionlocation[areaiterator].split(',')[0];
+            var templong = regionlocation[areaiterator].split(',')[1];
+            var temp_latLng = new google.maps.LatLng(templat, templong);
+            markers.push(new google.maps.Marker(
+            {
+                position: temp_latLng,
+                map: map,
+                icon: icons,
+                draggable: false
+            }));            
+            iterator++;
+            info(iterator);
+            areaiterator++;
+        }
+
+        function info(i) {
+            infowindow[i] = new google.maps.InfoWindow({
+                content: contentstring[i - 1]
+            });
+            infowindow[i].content = contentstring[i - 1];
+            google.maps.event.addListener(markers[i - 1], 'click', function() {
+                for (var j = 1; j < contentstring.length + 1; j++) {
+                    infowindow[j].close();
+                }
+                infowindow[i].open(map, markers[i - 1]);
+            });
+        }
+    </script>
 </head>
 <body>
 <div class="container" style="margin-top:25px;">
@@ -54,7 +187,16 @@ $data = json_decode($data, true);
 		<div class="col-md-2"></div>
 	</div>
 	<br />
-	<div class="row">
+	<div class="row" id='map'>
+        <div class="col-md-1"></div>
+        <div class="col-md-10 col-sm-12 col-xs-12">
+            <div id="Map" style="width: 100%;height: 500px;"></div>
+            <button id="list_btn"><i class="fa fa-bars fa-1x" style="color: #545454;"></i></button>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+	<div class="row" style="display:none;" id="list">
 		<div class="col-md-2">
 			<div style="display:none;"><?php echo "http://hospitalsiq.herokuapp.com/search/web/surajjana/".$_POST['lat']."/".$_POST['lon']."/".urlencode($dept)."/20"; ?></div>
 		</div>
@@ -91,6 +233,14 @@ $data = json_decode($data, true);
 							</div>';
 				}
 			?>
+			<div class="row">
+                <div class="col-md-12 hidden-sm hidden-xs">
+                    <button id='map_btn'><i class="fa fa-map"></i></button>
+                </div>
+                <div class="col-sm-12 col-xs-12 hidden-lg hidden-md">
+                    <button id='map_btn_mob'><i class="fa fa-map"></i></button>
+                </div>
+            </div>
 		</div>
 		<div class="col-md-2"></div>
 	</div>
