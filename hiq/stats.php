dfbb2f5e74eb18ff0d6b679b4826a4a282d71908
Suraj Jana <surajjana2@gmail.com>
2016-06-25 22:25:06 +0530

    new commit

---
+++ hiq/stats.php
@@ -0,0 +1,28 @@
+<?php 
+
+$fd = fopen("stats.json", "r");
+$json_data = fread($fd, filesize("stats.json"));
+$json_data = json_decode($json_data, true);
+
+/*$val = array("ip"=>"8.8.8.8","time"=>"");
+array_push($json_data,$val);*/
+
+$details = json_decode(file_get_contents("http://ipinfo.io/json"), ture);
+
+$val = array("ip" => $details["ip"],
+				"hostname" => $details["hostname"],
+				"city" => $details["city"],
+				"region" => $details["region"],
+				"country" => $details["country"],
+				"loc" => $details["loc"],
+				"org" => $details["org"],
+				"postal" => $details["postal"],
+				"time" => time());
+array_push($json_data,$val);
+
+$res = json_encode($json_data);
+
+$fl = fopen("stats.json", "w");
+fwrite($fl,$res);
+echo $res;
+?>
